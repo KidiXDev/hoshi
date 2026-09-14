d2128989bb9f90e7665ac0e29c940898a409fcd9
KidiXDev <dikhiardian55@gmail.com>
2026-09-14 19:09:29 +0700

    repo

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,3 +1,3 @@
 export const BRAND_NAME = 'Hoshi';
-export const REPO_SLUG = 'KidiXDev/comfy-gui';
+export const REPO_SLUG = 'KidiXDev/hoshi';
 export const REPO_URL = `https://github.com/${REPO_SLUG}`;
